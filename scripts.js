9fdb3a7065d3a4d6b1f7902984da914662bb2edb
GustavoFFelipe <gustavo.ffelipedev@gmail.com>
2022-05-18 20:48:45 -0300

    handle letters and validation

--- scripts.js
+++ scripts.js
@@ -3,43 +3,18 @@
   const showButton = document.getElementById('first-page');
   const showAddWord = document.getElementById('addWord');
 
-  const secretWords = ['CASA', 'CAMELO', 'HORIZONTE', 'FORTE', 'VERMELHO', 'ZEBRA', 'ASFALTO', 'LUA', 'SOL', 'NUVEM', 'ARVORES', 'HOJE', 'ONTEM', 'AMANHA','FLORES', 'PLANTAS', 'ANIMAIS', 'NUMEROS', 'CAVALEIRO', 'DAMA', 'SOLDADO', 'XADREZ', 'ESQUILO', 'FOCA', 'PIRATA']
-  const word= secretWords[Math.floor(Math.random() * secretWords.length)];
+  const secretWords = ['CASA', 'CAMELO', 'HORIZONTE', 'FORTE', 'VERMELHO', 'ZEBRA', 'ASFALTO', 'LUA', 'SOL', 'NUVEM', 'ARVORES', 'HOJE', 'ONTEM', 'AMANHA','FLORES', 'PLANTAS', 'ANIMAIS', 'NUMEROS', 'CAVALEIRO', 'DAMA', 'SOLDADO', 'XADREZ', 'ESQUILO', 'FOCA', 'PIRATA'];
   const wrongLetter = [];
   const rightLetter = [];
-  
-console.log(word)
-  //---------validation of words ---------------------//
-  document.addEventListener('keydown', (evento) => {
  
-    const codLetter = evento.keyCode; // 65 - 90 interval to alfabet letters
-    if(isLetter(codLetter)){
-      const letter = evento.key.toLocaleUpperCase('PT-BR');
-    console.log(letter)
-      if(wrongLetter.includes(letter)){
-        alert('type again')
-      }else {
-        if(word.includes(letter)){
-          rightLetter.push(letter)
-        } else {
-            wrongLetter.push(letter)
-        }
-      }
-      console.log(rightLetter)
-      console.log(wrongLetter)
+  //----------------------Random words --------------------------//
 
-  }
+function randomWord(arr){
+  return arr[Math.floor(Math.random() * arr.length)];
 }
-  )
 
-  function showAlert(){
-    const showAlert = document.getElementById('hidden-div');
-    showAlert.classList.add('show');
-  }
-
-  function isLetter(codLetter) {
-    return codLetter >= 65 && codLetter <= 90
-  }
+let word = randomWord(secretWords)
+console.log(word)
 //------------------ CHange display buttons to game ------------------//
 function handleShowGame() {
   if(showButton.style.display === 'flex') {
@@ -49,25 +24,68 @@ function handleShowGame() {
     showButton.style.display = 'flex';
     showGame.style.display = 'none'
   }
- drawSpacesCanvas()
+
+ drawSpacesCanvas();
 }
-// add only for test a bug that I try to solve
+handleShowGame() // add only for test a bug that I try to solve
+
+//---------validation of words ---------------------//
+
+document.addEventListener('keydown', (evento) => {
+
+  const codLetter = evento.keyCode; // 65 - 90 interval to alfabet letters
+  if(isLetter(codLetter)){
+    const letter = evento.key.toLocaleUpperCase('PT-BR');
+  console.log(letter)
+      if(word.includes(letter)){
+        rightLetter.push(letter)
+      } else {
+          wrongLetter.push(letter)
+      }
+    }
+    console.log(rightLetter)
+    console.log(wrongLetter)
+      updateGame();
+})
+function isLetter(codLetter) {
+  return codLetter >= 65 && codLetter <= 90
+}
+
+//---------------Update Game -----------------//
+function updateGame(){
+
+  showWrongLetter();
+}
+//-----------------Handdle letters ---------------//
+
+function showWrongLetter() {
+  const wrongContainer  = document.getElementById('wrong-letters');
+  wrongContainer.innerHTML = "";
+  wrongLetter.forEach( letter => {
+    wrongContainer.innerHTML += `<span>${letter}</span>`;
+  })
+}
+
+function showRightLetter(){
+  //  BEGIN FOR HERE
+}
+
 //-----------------adding new words to array ----------------//
 function addNewWord(){
-  const newWord = document.getElementById('text-area').value;
+  const newWord = document.getElementById('text-area').value.toLocaleUpperCase('PT-BR');
 
-  if(newWord ===""){
-    alert("Please enter only letters")
-  } else{
+  if( newWord !== ""){
     secretWords.push(newWord);
-   
-
-    handleAddWord()
+  } else{
+    alert('Please some word')
   }
+  console.log(newWord)
+  handleAddWord()
 }
 
 //-------------------- back to the buttons display(main) ----------------///
 function backDisplay(){
+  newGame()
   if(showButton.style.display === 'flex') {
     showButton.style.display = 'none';
     showGame.style.display = 'block'
@@ -79,37 +97,32 @@ function backDisplay(){
 
 //----------------- Handle with display functions to cancel the  adding word------------------//
 function handleAddWord(){
-
-  if(showButton.style.display === 'flex'){
+  const newWord = document.getElementById('text-area').value
+  if(showAddWord.style.display === 'block' &&  newWord !== ""){
+    showAddWord.style.display = 'none';
+    showButton.style.display = 'flex'
+  } else {
     showButton.style.display = 'none';
     showAddWord.style.display = 'block'
-  } else {
-    showButton.style.display = 'flex';
-    showAddWord.style.display = 'none'
   }
+
 }
 
 //--------------------------Canvas Script ---------------------------//
 
 function drawSpacesCanvas(){
-  console.log(word.length)
-
-
 const screen = document.querySelector("canvas");
 const draw = screen.getContext('2d');
-
 let space = 50;
 for( i = 0; i< word.length; i++) {
   draw.fillStyle = '#0A3871';
   draw.fillRect(space, 130, 20, 1);
   space = space + 27;
 }
-    
+
 }
-
-
-
-
-
-
+//------------------Function for new game -------------*/
+function newGame(){
+  document.location.reload(true);
+}
 
